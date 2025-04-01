58ab130c2a1fd1531f70870c84dd0361ba0ecfa0
PicturePark1101 <seojin5565@gmail.com>
2025-04-01 20:06:29 +0900

    refactor : 불필요한 코드 제거 #318

--- src/main/java/umc/th/juinjang/api/limjang/service/NoteCommandServiceV2.java
+++ src/main/java/umc/th/juinjang/api/limjang/service/NoteCommandServiceV2.java
@@ -43,24 +43,4 @@ public class NoteCommandServiceV2 {
 			request.sido(), request.sigungu(),
 			request.bname1(), request.bname2());
 	}
-
-	// public static LimjangPrice determineLimjangPrice(List<String> priceList, Integer purpose, Integer priceType){
-	// 	checkExpectedSize(priceType, priceList.size());
-	// 	if (purpose == 0){ // 부동산 투자 목적 -> 실거래가
-	// 		return LimjangPrice.builder().marketPrice(priceList.get(0)).build();
-	// 	} else if (purpose == 1){ // 직접 거래 목적
-	// 		switch (priceType){
-	// 			case 0 : // 매매
-	// 				return LimjangPrice.builder().sellingPrice(priceList.get(0)).build();
-	// 			case 1 :// 전세
-	// 				return LimjangPrice.builder().pullRent(priceList.get(0)).build();
-	// 			case 2 : // 월세 : 0, 보증금 : 1 이 경우 배열 길이는 무조건 2여야만 함.
-	// 				return LimjangPrice.builder().depositPrice(priceList.get(0))
-	// 					.monthlyRent(priceList.get(1)).build();
-	// 			case 3 :
-	// 				return LimjangPrice.builder().marketPrice(priceList.get(0)).build();
-	// 		}
-	// 	}
-	// 	return null;
-	// }
 }
