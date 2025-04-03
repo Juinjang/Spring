ac6c3e5524dde6f286810a349dd8ef7f3e64bcda
PicturePark1101 <seojin5565@gmail.com>
2025-04-03 19:58:33 +0900

    refactor : 임장 post Request DTO 내부에 toEntity 로직 생성 #319

--- src/main/java/umc/th/juinjang/api/limjang/controller/request/NotePostRequest.java
+++ src/main/java/umc/th/juinjang/api/limjang/controller/request/NotePostRequest.java
@@ -3,9 +3,16 @@ package umc.th.juinjang.api.limjang.controller.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import umc.th.juinjang.common.code.status.ErrorStatus;
+import umc.th.juinjang.common.exception.handler.LimjangHandler;
+import umc.th.juinjang.domain.limjang.model.Address;
+import umc.th.juinjang.domain.limjang.model.Limjang;
+import umc.th.juinjang.domain.limjang.model.LimjangPrice;
 import umc.th.juinjang.domain.limjang.model.LimjangPriceType;
 import umc.th.juinjang.domain.limjang.model.LimjangPropertyType;
 import umc.th.juinjang.domain.limjang.model.LimjangPurpose;
+import umc.th.juinjang.domain.limjang.repository.NotePriceFactory;
+import umc.th.juinjang.domain.member.model.Member;
 
 public record NotePostRequest(
 	@NotNull
@@ -34,4 +41,11 @@ public record NotePostRequest(
 	String bname1,
 	String bname2
 ) {
+	public Limjang toEntity(Member member) {
+		LimjangPrice limjangPrice = NotePriceFactory.create(purposeType, priceType, price, monthlyRent);
+		Address address = Address.create(roadAddress, addressDetail, bcode, sido, sigungu, bname1, bname2);
+
+		return Limjang.create(member, limjangPrice, purposeType, propertyType, priceType, nickname, address, pyong,
+			floor);
+	}
 }
