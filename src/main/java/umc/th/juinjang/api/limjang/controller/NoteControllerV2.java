f3befac67c9f9eff852168eb1721987725233f3c
PicturePark1101 <seojin5565@gmail.com>
2025-04-04 23:15:05 +0900

    feat : 마이노트 임장 조회 controller 구현 #321

--- src/main/java/umc/th/juinjang/api/limjang/controller/NoteControllerV2.java
+++ src/main/java/umc/th/juinjang/api/limjang/controller/NoteControllerV2.java
@@ -1,17 +1,22 @@
 package umc.th.juinjang.api.limjang.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import umc.th.juinjang.api.dto.ApiResponse;
+import umc.th.juinjang.api.limjang.controller.parameter.LimjangSortOptions;
 import umc.th.juinjang.api.limjang.controller.request.NotePostRequest;
 import umc.th.juinjang.api.limjang.service.NoteCommandServiceV2;
+import umc.th.juinjang.api.limjang.service.NoteQueryServiceV2;
+import umc.th.juinjang.api.limjang.service.response.UserNotesGetResponse;
 import umc.th.juinjang.common.code.status.SuccessStatus;
 import umc.th.juinjang.domain.member.model.Member;
 
@@ -21,6 +26,7 @@ import umc.th.juinjang.domain.member.model.Member;
 public class NoteControllerV2 {
 
 	private final NoteCommandServiceV2 noteCommandService;
+	private final NoteQueryServiceV2 noteQueryService;
 
 	@Operation(summary = "임장 생성 API V2")
 	@PostMapping
@@ -29,4 +35,13 @@ public class NoteControllerV2 {
 		noteCommandService.createNote(request, member);
 		return ApiResponse.of(SuccessStatus._CREATED, null);
 	}
+
+	@Operation(summary = "마이 노트 조회 API V2")
+	@GetMapping
+	public ApiResponse<UserNotesGetResponse> findUsersNotes(
+		@RequestParam("sort") LimjangSortOptions sortOptions,
+		@AuthenticationPrincipal Member member) {
+		return ApiResponse.onSuccess(noteQueryService.findUsersNotes(member, sortOptions));
+	}
+
 }
