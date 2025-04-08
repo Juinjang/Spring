00207bcc8cb87f073188b1bbbe3d440914278093
sonjs6789 <sonjs7554@naver.com>
2025-04-08 19:22:05 +0900

    feat : 닉네임 중복 여부에 대한 api 코드 완성
    - 해당 api는 권한 체크하지 않도록 SecurityConfig 수정

--- src/main/java/umc/th/juinjang/auth/config/SecurityConfig.java
+++ src/main/java/umc/th/juinjang/auth/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package umc.th.juinjang.auth.config;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -16,85 +17,87 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import lombok.RequiredArgsConstructor;
 import umc.th.juinjang.auth.jwt.JwtAuthenticationFilter;
 import umc.th.juinjang.auth.jwt.JwtExceptionFilter;
 import umc.th.juinjang.auth.jwt.JwtService;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final AuthenticationConfiguration authenticationConfiguration;
+	private final AuthenticationConfiguration authenticationConfiguration;
 
-    private final JwtService jwtService;
+	private final JwtService jwtService;
 
-    private final JwtExceptionFilter jwtExceptionFilter;
+	private final JwtExceptionFilter jwtExceptionFilter;
 
-    private final Environment environment;
-    @Bean
-    @Order(0)
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        String[] activeProfiles = environment.getActiveProfiles();
-        boolean isProd = Arrays.asList(activeProfiles).contains("prod");
+	private final Environment environment;
 
-        //prod아닐때
-        if (!isProd) {
-            return web -> web.ignoring()
-                    .requestMatchers("/swagger-ui/**", "/swagger/**", "/swagger-resources/**", "/swagger-ui.html", "/test",
-                            "/configuration/ui",  "/v3/api-docs/**", "/h2-console/**", "/api/auth/regenerate-token",
-                            "/api/auth/kakao/**", "/api/auth/apple/**", "/actuator/prometheus",
-                            "/api/auth/v2/apple/**", "/api/auth/v2/kakao/**");
-        }
-        else {
-          return web -> web.ignoring()
-                    .requestMatchers("/h2-console/**", "/api/auth/regenerate-token",
-                            "/api/auth/kakao/**", "/api/auth/apple/**", "/actuator/prometheus",
-                            "/api/auth/v2/apple/**", "/api/auth/v2/kakao/**");
-        }
+	@Bean
+	@Order(0)
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		String[] activeProfiles = environment.getActiveProfiles();
+		boolean isProd = Arrays.asList(activeProfiles).contains("prod");
 
-    }
+		//prod아닐때
+		if (!isProd) {
+			return web -> web.ignoring()
+				.requestMatchers("/swagger-ui/**", "/swagger/**", "/swagger-resources/**", "/swagger-ui.html", "/test",
+					"/configuration/ui", "/v3/api-docs/**", "/h2-console/**", "/api/auth/regenerate-token",
+					"/api/auth/kakao/**", "/api/auth/apple/**", "/actuator/prometheus",
+					"/api/auth/v2/apple/**", "/api/auth/v2/kakao/**", "/api/members/nickname/exists");
+		} else {
+			return web -> web.ignoring()
+				.requestMatchers("/h2-console/**", "/api/auth/regenerate-token",
+					"/api/auth/kakao/**", "/api/auth/apple/**", "/actuator/prometheus",
+					"/api/auth/v2/apple/**", "/api/auth/v2/kakao/**", "/api/members/nickname/exists");
+		}
 
-    //선언 방식이 3.x에서 바뀜
-    @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception
-    { return authConfiguration.getAuthenticationManager(); }
+	}
 
-    @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	//선언 방식이 3.x에서 바뀜
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+		return authConfiguration.getAuthenticationManager();
+	}
 
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(Customizer.withDefaults())
-                .sessionManagement((sessionManagement) ->
-                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                        세션을 사용하지 않는다고 설정함
-                )
-                .addFilter(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration),jwtService))
-//                 JwtAuthenticationFilter를 필터에 넣음
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers(
-                                        AntPathRequestMatcher.antMatcher("/api/auth/**")
-                                ).authenticated()
-                                .requestMatchers(
-                                    AntPathRequestMatcher.antMatcher("/h2-console/**")
-                                ).permitAll()
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-                                .anyRequest().authenticated()
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(Customizer.withDefaults())
+			.sessionManagement((sessionManagement) ->
+					sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				//                        세션을 사용하지 않는다고 설정함
+			)
+			.addFilter(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration), jwtService))
+			//                 JwtAuthenticationFilter를 필터에 넣음
+			.authorizeHttpRequests((authorizeRequests) ->
+				authorizeRequests
+					.requestMatchers(
+						AntPathRequestMatcher.antMatcher("/api/members/nickname/exists"),
+						AntPathRequestMatcher.antMatcher("/h2-console/**")
+					).permitAll()
+					.requestMatchers(
+						AntPathRequestMatcher.antMatcher("/api/auth/**")
+					).authenticated()
+					.anyRequest().authenticated()
 
-                )
-                .headers(
-                        headersConfigurer ->
-                                headersConfigurer
-                                        .frameOptions(
-                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                                        )
-                )
-                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+			)
+			.headers(
+				headersConfigurer ->
+					headersConfigurer
+						.frameOptions(
+							HeadersConfigurer.FrameOptionsConfig::sameOrigin
+						)
+			)
+			.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
 
-        return http.build();
-    }
+		return http.build();
+	}
 
 }
 
