86dbf143100ed7b23b27bff334a920a36a3c4b44
PicturePark1101 <seojin5565@gmail.com>
2025-04-10 19:16:19 +0900

    feat : service 구현 - 이미지 id와 임장 id 매핑해주는 map 함수 구현 #322

--- src/main/java/umc/th/juinjang/api/limjang/service/NoteQueryServiceV2.java
+++ src/main/java/umc/th/juinjang/api/limjang/service/NoteQueryServiceV2.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import umc.th.juinjang.api.image.service.ImageFinder;
 import umc.th.juinjang.api.limjang.controller.parameter.LimjangSortOptions;
+import umc.th.juinjang.api.limjang.service.response.UserNotesShareableGetResponse;
 import umc.th.juinjang.api.limjang.service.response.UserNotesGetResponse;
 import umc.th.juinjang.api.scrap.service.ScarpFinder;
+import umc.th.juinjang.domain.image.model.Image;
 import umc.th.juinjang.domain.limjang.model.Limjang;
 import umc.th.juinjang.domain.member.model.Member;
 
@@ -22,6 +25,7 @@ public class NoteQueryServiceV2 {
 
 	private final NoteFinder noteFinder;
 	private final ScarpFinder scarpFinder;
+	private final ImageFinder imageFinder;
 
 	@Transactional(readOnly = true)
 	public UserNotesGetResponse findUsersNotes(Member member, LimjangSortOptions sortOptions) {
@@ -42,5 +46,23 @@ public class NoteQueryServiceV2 {
 			.stream()
 			.map(it -> it.getLimjangId().getLimjangId())
 			.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public UserNotesShareableGetResponse findNotesShareable(Member member) {
+		List<Limjang> notes = noteFinder.getAllByMemberWithAddressAndNotePriceWhereRewardPencilIsNotNullAndDeletedIsFalse(
+			member);
+
+		List<Image> imageList = imageFinder.findAllFirstCreatedImagePerNote(notes);
+
+		return UserNotesShareableGetResponse.of(notes, mapToNoteIdAndImageId(imageList), mapToNoteScrapStatus(notes));
+	}
+
+	private Map<Long, String> mapToNoteIdAndImageId(List<Image> imageList) {
+		return imageList.stream()
+			.collect(Collectors.toMap(
+				image -> image.getLimjangId().getLimjangId(),
+				image -> image.getImageUrl()
+			));
 	}
 }
