c261beedd9fc9a7a31ccd5798e9ce134aed36e43
PicturePark1101 <seojin5565@gmail.com>
2025-04-18 20:11:30 +0900

    feat : 얻은 연필 관련 리포지토리 설정 #341

---
+++ src/main/java/umc/th/juinjang/domain/pencil/acquired/repository/AcquiredPencilRepository.java
@@ -0,0 +1,8 @@
+package umc.th.juinjang.domain.pencil.acquired.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import umc.th.juinjang.domain.pencil.acquired.model.AcquiredPencil;
+
+public interface AcquiredPencilRepository extends JpaRepository<AcquiredPencil, Long> {
+}
