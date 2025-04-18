5766a65e9290d26e3db7ed5944eaf2d36a6b8f9e
PicturePark1101 <seojin5565@gmail.com>
2025-04-18 20:25:30 +0900

    feat : 이미 구매한 임장인지 확인 #341

---
+++ src/main/java/umc/th/juinjang/api/pencil/service/UsedPencilFinder.java
@@ -0,0 +1,19 @@
+package umc.th.juinjang.api.pencil.service;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import umc.th.juinjang.domain.member.model.Member;
+import umc.th.juinjang.domain.pencil.used.repository.UsedPencilRepository;
+
+@Component
+@RequiredArgsConstructor
+public class UsedPencilFinder {
+
+	private final UsedPencilRepository usedPencilRepository;
+
+	public boolean existsByMemberAndSharedNoteId(Member member, long sharedNoteId) {
+		return usedPencilRepository.existsByMemberAndSharedNoteId(member, sharedNoteId);
+	}
+
+}
