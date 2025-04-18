b4716cb36fed522ca88b0616b861434cb1bb90e2
PicturePark1101 <seojin5565@gmail.com>
2025-04-18 20:23:15 +0900

    feat : 구매 관련 errorstatus 추가 #341

--- src/main/java/umc/th/juinjang/common/code/status/ErrorStatus.java
+++ src/main/java/umc/th/juinjang/common/code/status/ErrorStatus.java
@@ -106,8 +106,9 @@ public enum ErrorStatus implements BaseErrorCode {
 	PENCIL_ACCOUNT_NOT_FOUND(HttpStatus.BAD_REQUEST, "ACCOUNT4000", "멤버에 해당하는 계좌가 존재하지 않습니다."),
 
 	SHAREDNOTE_NOT_FOUND(HttpStatus.BAD_REQUEST, "SHAREDNOTE4000", "해당하는 공유노트가 존재하지 않습니다."),
-	SHAREDNOTE_ALREADY_PURCHASE(HttpStatus.BAD_REQUEST, "SHAREDNOTE4001", "보유한 연필 수가 부족합니다."),
-	SHAREDNOTE_CONFLICT(HttpStatus.CONFLICT, "SHAREDNOTE4002", "이미 구매한 노트입니다.");
+	SHAREDNOTE_NOT_ENOUGH_PENCIL(HttpStatus.BAD_REQUEST, "SHAREDNOTE4001", "보유한 연필 수가 부족합니다."),
+	SHAREDNOTE_CONFLICT(HttpStatus.CONFLICT, "SHAREDNOTE4002", "이미 구매한 노트입니다."),
+	SHAREDNOTE_DEADLOCK(HttpStatus.LOCKED, "SHAREDNOTE4003", "잠시 후 다시 시도해주세요. 현재 다른 요청이 처리 중입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
