d64c2ecd07d56e019b20f5a7e8625ea2e5716428
PicturePark1101 <seojin5565@gmail.com>
2025-04-24 19:53:25 +0900

    feat : 좋아요 관련 로직 에러 처리 #343

---
+++ src/main/java/umc/th/juinjang/common/exception/handler/LikedNoteHandler.java
@@ -0,0 +1,10 @@
+package umc.th.juinjang.common.exception.handler;
+
+import umc.th.juinjang.common.code.BaseErrorCode;
+import umc.th.juinjang.common.exception.GeneralException;
+
+public class LikedNoteHandler extends GeneralException {
+	public LikedNoteHandler(BaseErrorCode code) {
+		super(code);
+	}
+}
