56d9e2e61a67fa08e8c91d8a52a2048ea6d29604
PicturePark1101 <seojin5565@gmail.com>
2025-04-30 21:27:48 +0900

    fix : 구매 연필 사용 시 PurchasedPencil에도 반영되도록 fix #357

---
+++ src/main/java/umc/th/juinjang/api/pencil/service/PurchasedPencilUpdater.java
@@ -0,0 +1,23 @@
+package umc.th.juinjang.api.pencil.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import umc.th.juinjang.domain.member.model.Member;
+import umc.th.juinjang.domain.pencil.purchased.model.PurchasedPencil;
+import umc.th.juinjang.domain.pencil.purchased.repository.PurchasedPencilRepository;
+
+@Component
+@RequiredArgsConstructor
+public class PurchasedPencilUpdater {
+
+	private PurchasedPencilRepository purchasedPencilRepository;
+
+	public List<PurchasedPencil> findByMemberAndRemainQuantityGreaterThanOrderByCreatedAtAsc(Member buyer,
+		Long remainQuantity) {
+		return purchasedPencilRepository.findByMemberAndRemainQuantityGreaterThanOrderByCreatedAtAsc(buyer,
+			remainQuantity);
+	}
+}
