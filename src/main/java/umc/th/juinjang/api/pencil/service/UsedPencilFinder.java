0bafb4151ad33d0e0e4e70627904316bd9f72323
PicturePark1101 <seojin5565@gmail.com>
2025-04-30 19:39:20 +0900

    feat : 구매여부판별 쿼리 #355

--- src/main/java/umc/th/juinjang/api/pencil/service/UsedPencilFinder.java
+++ src/main/java/umc/th/juinjang/api/pencil/service/UsedPencilFinder.java
@@ -1,5 +1,7 @@
 package umc.th.juinjang.api.pencil.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,10 @@ public class UsedPencilFinder {
 
 	public int countBySharedNoteId(long sharedNoteId) {
 		return usedPencilRepository.countBySharedNoteId(sharedNoteId);
+	}
+
+	public List<Long> findByMemberInSharedNoteIdsAndTypeIsOwned(Member member, List<Long> sharedNoteIds) {
+		return usedPencilRepository.findByMemberInSharedNoteIdsAndTypeIsOwned(member, sharedNoteIds);
 	}
 
 }
--- src/main/java/umc/th/juinjang/domain/pencil/used/repository/UsedPencilRepository.java
+++ src/main/java/umc/th/juinjang/domain/pencil/used/repository/UsedPencilRepository.java
@@ -1,6 +1,10 @@
 package umc.th.juinjang.domain.pencil.used.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import umc.th.juinjang.domain.member.model.Member;
@@ -12,4 +16,8 @@ public interface UsedPencilRepository extends JpaRepository<UsedPencil, Long> {
 	boolean existsByMemberAndSharedNoteId(Member member, Long sharedNoteId);
 
 	int countBySharedNoteId(Long sharedNoteId);
+
+	@Query("select u.sharedNoteId from UsedPencil u where u.member = :member and u.sharedNoteId in :sharedNoteIds and u.type = 'OWNED'")
+	List<Long> findByMemberInSharedNoteIdsAndTypeIsOwned(@Param("member") Member member,
+		@Param("sharedNoteIds") List<Long> sharedNoteIds);
 }
