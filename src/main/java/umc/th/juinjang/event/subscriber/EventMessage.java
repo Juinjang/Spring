12efec0846f9d2741c5ea42952c0216187001ebe
PicturePark1101 <seojin5565@gmail.com>
2025-05-16 17:22:56 +0900

    feat : 공유노트 신고 시 디스코드 알림 가도록 이벤트 리스너 구현 #373

--- src/main/java/umc/th/juinjang/event/subscriber/EventMessage.java
+++ src/main/java/umc/th/juinjang/event/subscriber/EventMessage.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public enum EventMessage {
-  SIGN_UP_MESSAGE("주인장에 %s %d번째 유저 < %s >님이 생겼어요!");
+	SIGN_UP_MESSAGE("주인장에 %s %d번째 유저 < %s >님이 생겼어요!"),
+	FLAG_SHARED_NOTE_MESSAGE("< %d >번 유저가 [ %s ]의 사유로 < %d >번 유저의 < %d >번 공유 노트를 신고했습니다.");
 
-  private final String message;
+	private final String message;
 }
