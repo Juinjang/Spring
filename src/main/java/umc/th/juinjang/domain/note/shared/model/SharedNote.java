9006914a4a8207ff581f0bdebcc5541b2cd402ee
현아 <52688527+hyeonahhh@users.noreply.github.com>
2025-06-20 19:44:56 +0900

    🐛 fix: sharedNote에 price 필드값 업데이트

--- src/main/java/umc/th/juinjang/domain/note/shared/model/SharedNote.java
+++ src/main/java/umc/th/juinjang/domain/note/shared/model/SharedNote.java
@@ -73,7 +73,7 @@ public class SharedNote extends BaseEntity {
 		return this.likeCount = (likeCount == null ? 1L : likeCount + 1);
 	}
 
-	public static SharedNote toSharedNote(Member member, Limjang limjang, SharedNotePostRequest dto) {
+	public static SharedNote toSharedNote(Member member, Limjang limjang, SharedNotePostRequest dto, Long price) {
 		return SharedNote.builder()
 			.member(member)
 			.limjang(limjang)
@@ -85,6 +85,7 @@ public class SharedNote extends BaseEntity {
 			.isImageShared(dto.isImageShared())
 			.viewCount(0L)
 			.likeCount(0L)
+			.price(price)
 			.build();
 	}
 
