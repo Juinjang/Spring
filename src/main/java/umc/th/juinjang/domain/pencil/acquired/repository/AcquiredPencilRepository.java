4d9712b3ba79a93d441d31be1c75b0513cc923bc
sonjs6789 <sonjs7554@naver.com>
2025-07-06 22:04:24 +0900

    fix : 얻은 연필 목록에서 buildingName 같이 넘기도록 수정

--- src/main/java/umc/th/juinjang/domain/pencil/acquired/repository/AcquiredPencilRepository.java
+++ src/main/java/umc/th/juinjang/domain/pencil/acquired/repository/AcquiredPencilRepository.java
@@ -3,13 +3,25 @@ package umc.th.juinjang.domain.pencil.acquired.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import umc.th.juinjang.api.pencil.service.response.AcquiredPencilResponse;
 import umc.th.juinjang.domain.member.model.Member;
 import umc.th.juinjang.domain.pencil.acquired.model.AcquiredPencil;
 
 public interface AcquiredPencilRepository extends JpaRepository<AcquiredPencil, Long> {
-	List<AcquiredPencil> findAllByMemberOrderByCreatedAtDesc(Member member);
+
+	@Query("SELECT new umc.th.juinjang.api.pencil.service.response.AcquiredPencilResponse(" +
+		"ap.id, ap.content, ap.sharedNoteId, ap.acquiredQuantity, " +
+		"ap.isRead, ap.type, ap.createdAt, sn.buildingName) " +
+		"FROM AcquiredPencil ap " +
+		"LEFT JOIN SharedNote sn ON ap.sharedNoteId = sn.sharedNoteId " +
+		"WHERE ap.member = :member " +
+		"ORDER BY ap.createdAt DESC")
+	List<AcquiredPencilResponse> findAllByMemberWithBuildingNameOrderByCreatedAtDesc(@Param("member") Member member);
 
 	boolean existsByMemberAndIsReadFalse(Member member);
+
 	boolean existsByMember(Member member);
 }
