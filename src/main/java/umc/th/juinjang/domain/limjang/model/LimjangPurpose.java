04ff81fc8086ae1e6c89fd002f980d2534f0fa91
sonjusun <sonjusun@sonjusunui-MacBookPro.local>
2025-09-18 22:26:31 +0900

    feat : 임장 생성 ( INIT ) 구현 완료 및 컨트롤러 테스트 구현
    - UX 개선을 위한 임장 생성 분리 ( 간단한 임장 생성 구현 )

--- src/main/java/umc/th/juinjang/domain/limjang/model/LimjangPurpose.java
+++ src/main/java/umc/th/juinjang/domain/limjang/model/LimjangPurpose.java
@@ -1,30 +1,31 @@
 package umc.th.juinjang.domain.limjang.model;
 
 import java.util.Arrays;
+
 import umc.th.juinjang.common.code.status.ErrorStatus;
 import umc.th.juinjang.common.exception.handler.LimjangHandler;
 
 public enum LimjangPurpose {
 
-  INVESTMENT(0),  // 투자 목적
-  RESIDENTIAL_PURPOSE(1); // 거주 목적
+	INVESTMENT(0),  // 투자 목적
+	RESIDENTIAL_PURPOSE(1); // 거주 목적, 직접 입주
 
-  private final int value;
+	private final int value;
 
-  LimjangPurpose(int value) {
-    this.value = value;
-  }
+	LimjangPurpose(int value) {
+		this.value = value;
+	}
 
-  // 숫자 리턴
-  public int getValue() {
-    return value;
-  }
+	// 숫자 리턴
+	public int getValue() {
+		return value;
+	}
 
-  public static LimjangPurpose find(int inputValue) {
-    return Arrays.stream(LimjangPurpose.values())
-        .filter(it -> it.value == inputValue)
-        .findAny()
-        .orElseThrow(() -> new LimjangHandler(ErrorStatus.LIMJANG_POST_TYPE_ERROR));
-  }
+	public static LimjangPurpose find(int inputValue) {
+		return Arrays.stream(LimjangPurpose.values())
+			.filter(it -> it.value == inputValue)
+			.findAny()
+			.orElseThrow(() -> new LimjangHandler(ErrorStatus.LIMJANG_POST_TYPE_ERROR));
+	}
 
 }
