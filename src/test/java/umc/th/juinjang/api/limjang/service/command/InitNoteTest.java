2b709c31ccbe597ea2a840776daebe04a572f0d6
sonjusun <sonjusun@sonjusunui-MacBookPro.local>
2025-10-28 00:45:11 +0900

    test: initNote 와 createNote 로 만들어진 CRUD 테스트 추가

--- src/test/java/umc/th/juinjang/api/limjang/service/command/InitNoteTest.java
+++ src/test/java/umc/th/juinjang/api/limjang/service/command/InitNoteTest.java
@@ -1,16 +1,22 @@
 package umc.th.juinjang.api.limjang.service.command;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import umc.th.juinjang.api.IntegrationTestSupport;
 import umc.th.juinjang.api.limjang.controller.parameter.LimjangSortOptions;
 import umc.th.juinjang.api.limjang.controller.request.NoteInitRequest;
+import umc.th.juinjang.api.limjang.controller.request.NotePatchRequest;
 import umc.th.juinjang.api.limjang.service.NoteCommandServiceV2;
 import umc.th.juinjang.api.limjang.service.NoteQueryServiceV2;
 import umc.th.juinjang.api.limjang.service.response.NotePostResponse;
@@ -25,16 +31,11 @@ import umc.th.juinjang.domain.member.model.Member;
 import umc.th.juinjang.domain.member.repository.MemberRepository;
 
 @Slf4j
+@DisplayName("노트 초기화 테스트")
 public class InitNoteTest extends IntegrationTestSupport {
-	Member firstMember;
-	Member secondMember;
-	Member thirdMember;
-
-	NotePostResponse noteInitResponse;
 
 	@Autowired
 	private MemberRepository memberRepository;
-
 	@Autowired
 	private NoteCommandServiceV2 noteCommandService;
 	@Autowired
@@ -42,121 +43,197 @@ public class InitNoteTest extends IntegrationTestSupport {
 	@Autowired
 	private LimjangRepository limjangRepository;
 
+	private Member firstMember;
+	private Member secondMember;
+	private Member thirdMember;
+
 	@BeforeEach
 	void setUp() {
 		flushAndTestUsers();
-
-		NoteInitRequest request = new NoteInitRequest(
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT,
-			"50000",
-			"4000"
-		);
-		noteInitResponse = noteCommandService.initNote(request, firstMember);
 	}
 
-	@Test
-	@DisplayName("UX 리팩토링을 위해, 새로운 노트 생성이 정상적으로 작동하는 가 ?")
-	void initNote() {
-		Long createNoteId = noteInitResponse.noteId();
+	@Nested
+	@DisplayName("간소화 된 생성을 했을 경우")
+	class InitNotes {  // 괄호 제거!
 
-		Limjang note = limjangRepository.findById(createNoteId).orElseThrow();
-		assertThat(note).extracting(
-			"memberId.memberId",
-			"purpose",
-			"propertyType",
-			"priceType",
-			"deleted",
-			"recordCount"
-		).containsExactly(
-			firstMember.getMemberId(),
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT,
-			false,
-			0
-		);
-		assertThat(note).extracting("limjangPrice").isNotNull();
-		assertThat(note).extracting("nickname").satisfies(n -> assertThat((String)n).endsWith(" 매물노트"));
-	}
+		private NotePostResponse noteInitResponse;
 
-	@Test
-	@DisplayName("initNote로 생성된 노트가 리스트에 정상 노출")
-	void initNoteAreVisibleInList() {
-		// when
-		log.info("=== 노트 생성 시작 ===");
-		Long createdNoteId = noteInitResponse.noteId();
-		log.info("생성된 노트 ID: {}", createdNoteId);
+		@BeforeEach
+		void setUpNote() {
+			NoteInitRequest request = new NoteInitRequest(
+				LimjangPurpose.RESIDENTIAL_PURPOSE,
+				LimjangPropertyType.APARTMENT,
+				LimjangPriceType.MONTHLY_RENT,
+				"50000",
+				"4000"
+			);
+			noteInitResponse = noteCommandService.initNote(request, firstMember);
+		}
 
-		log.info("=== 노트 조회 시작 ===");
-		UserNotesGetResponse response = noteQueryService.findUsersNotes(
-			firstMember, LimjangSortOptions.CREATED, ""
-		);
-		log.info("조회된 노트 개수: {}", response.notes().size());
-		// then
-		assertThat(response.notes()).hasSize(1);
+		@Test
+		@DisplayName("UX 리팩토링을 위해, 새로운 노트 생성이 정상적으로 작동하는 가 ?")
+		void initNote() {
+			// given
+			Long createNoteId = noteInitResponse.noteId();
 
-		UserNotesGetResponse.UserNoteResponse note = response.notes().get(0);
-		log.info("=== 노트 정보 ===");
-		log.info("### notes : {} ", note);
-		log.info("노트 ID: {}", note.noteId());
-		log.info("노트 이름: {}", note.name());
-		log.info("가격: {}", note.price());
-		log.info("월세: {}", note.monthlyRent());
+			// when
+			Limjang note = limjangRepository.findById(createNoteId).orElseThrow();
 
-		assertInitNoteBasicFields(note, createdNoteId);
-		assertInitNoteEmptyFields(note);
-	}
+			// then
+			assertAll(
+				() -> assertThat(note.getMemberId().getMemberId()).isEqualTo(firstMember.getMemberId()),
+				() -> assertThat(note.getPurpose()).isEqualTo(LimjangPurpose.RESIDENTIAL_PURPOSE),
+				() -> assertThat(note.getPropertyType()).isEqualTo(LimjangPropertyType.APARTMENT),
+				() -> assertThat(note.getPriceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+				() -> assertThat(note.getRecordCount()).isEqualTo(0),
+				() -> assertThat(note.getLimjangPrice()).isNotNull(),
+				() -> assertThat(note.getNickname()).endsWith(" 매물노트")
+			);
+		}
 
-	@Test
-	@DisplayName("initNote로 생성된 노트의 상세가 정상 노출")
-	void initNoteDetailAreVisible() {
-		// given
-		Long createdNoteId = noteInitResponse.noteId();
+		@Test
+		@DisplayName("initNote로 생성된 노트가 리스트에 정상 노출")
+		void initNoteAreVisibleInList() {
+			// given
+			log.info("=== 노트 생성 시작 ===");
+			Long createdNoteId = noteInitResponse.noteId();
+			log.info("생성된 노트 ID: {}", createdNoteId);
 
-		UserNoteGetResponse response = noteQueryService.findNote(createdNoteId);
-		log.info("### response : {} ", response);
-		
+			// when
+			log.info("=== 노트 조회 시작 ===");
+			UserNotesGetResponse response = noteQueryService.findUsersNotes(
+				firstMember, LimjangSortOptions.CREATED, ""
+			);
+			log.info("조회된 노트 개수: {}", response.notes().size());
+
+			// then
+			assertThat(response.notes()).hasSize(1);
+
+			UserNotesGetResponse.UserNoteResponse note = response.notes().get(0);
+			log.info("=== 노트 정보 ===");
+			log.info("### notes : {} ", note);
+			log.info("노트 ID: {}", note.noteId());
+			log.info("노트 이름: {}", note.name());
+			log.info("가격: {}", note.price());
+			log.info("월세: {}", note.monthlyRent());
+
+			assertAll(
+				() -> assertInitNoteBasicFields(note, createdNoteId),
+				() -> assertInitNoteEmptyFields(note)
+			);
+		}
+
+		@Test
+		@DisplayName("initNote로 생성된 노트의 상세가 정상 노출")
+		void initNoteDetailAreVisible() {
+			// given
+			Long createdNoteId = noteInitResponse.noteId();
+
+			// when
+			UserNoteGetResponse response = noteQueryService.findNote(createdNoteId);
+			log.info("### response : {} ", response);
+
+			// then
+			assertInitNoteBasicFields(response, createdNoteId);
+		}
+
+		@Test
+		@Transactional
+		@DisplayName("생성된 노트가 정상적으로 수정이 되는 가")
+		void noteByInited_CanbeUpdate() {
+			// given
+			Long createdNoteId = noteInitResponse.noteId();
+
+			NotePatchRequest updateRequest = new NotePatchRequest(
+				LimjangPriceType.MONTHLY_RENT,
+				"60000",  // 보증금 변경
+				"5000",   // 월세 변경
+				"서울특별시 강남구 테헤란로 123",  // 도로명 주소
+				"101동 101호",  // 상세 주소
+				"1168010100",   // 법정동코드
+				"강남 아파트 매물노트",  // 닉네임 변경
+				"5",      // 층수
+				32,       // 평수
+				"서울특별시",   // 시도
+				"강남구",       // 시군구
+				"역삼동",       // 법정동명1
+				""              // 법정동명2
+			);
+
+			// when
+			noteCommandService.updateNoteV2(createdNoteId, updateRequest);
+
+			// then
+			Limjang updatedNote = limjangRepository.findById(createdNoteId).orElseThrow();
+
+			assertAll(
+				() -> assertThat(updatedNote.getNickname()).isEqualTo("강남 아파트 매물노트"),
+				() -> assertThat(updatedNote.getPriceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+				() -> assertThat(updatedNote.getFloor()).isEqualTo("5"),
+				() -> assertThat(updatedNote.getPyong()).isEqualTo(32),
+				() -> assertThat(updatedNote.getLimjangPrice()).isNotNull(),
+				() -> assertThat(updatedNote.getLimjangPrice().getMonthlyRent()).isEqualTo("5000"),
+				() -> assertThat(updatedNote.getAddressEntity()).isNotNull(),
+				() -> assertThat(updatedNote.getAddressEntity().getRoadAddress()).isEqualTo("서울특별시 강남구 테헤란로 123"),
+				() -> assertThat(updatedNote.getAddressEntity().getAddressDetail()).isEqualTo("101동 101호")
+			);
+		}
+
+		@Test
+		@Transactional
+		@DisplayName("생성된 노트가 정상적으로 삭제가 되는 가")
+		void noteByInited_CanbeDeleted() {
+			// given
+			Long createdNoteId = noteInitResponse.noteId();
+			Limjang note = limjangRepository.findById(createdNoteId).orElseThrow();
+
+			// when
+			limjangRepository.softDeleteByIds(List.of(createdNoteId));
+			limjangRepository.flush();
+
+			// then
+			Limjang deletedNote = limjangRepository.findById(createdNoteId).orElseThrow();
+
+			// 삭제된 노트는 리스트에서 조회되지 않아야 함
+			UserNotesGetResponse response = noteQueryService.findUsersNotes(
+				firstMember, LimjangSortOptions.CREATED, ""
+			);
+			assertThat(response.notes()).isEmpty();
+		}
 	}
 
 	private void assertInitNoteBasicFields(UserNotesGetResponse.UserNoteResponse note, Long expectedId) {
-		assertThat(note).extracting(
-			"noteId", "purposeType", "propertyType", "priceType", "isScraped"
-		).containsExactly(
-			expectedId,
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT,
-			false
+		assertAll(
+			() -> assertThat(note.noteId()).isEqualTo(expectedId),
+			() -> assertThat(note.purposeType()).isEqualTo(LimjangPurpose.RESIDENTIAL_PURPOSE),
+			() -> assertThat(note.propertyType()).isEqualTo(LimjangPropertyType.APARTMENT),
+			() -> assertThat(note.priceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+			() -> assertThat(note.isScraped()).isFalse(),
+			() -> assertThat(note.name()).endsWith(" 매물노트"),
+			() -> assertThat(note.price()).isEqualTo("50000"),
+			() -> assertThat(note.monthlyRent()).isEqualTo("4000")
 		);
-
-		assertThat(note.name()).endsWith(" 매물노트");
-		assertThat(note.price()).isEqualTo("50000");
-		assertThat(note.monthlyRent()).isEqualTo("4000");
 	}
 
 	private void assertInitNoteBasicFields(UserNoteGetResponse note, Long expectedId) {
-		assertThat(note).extracting(
-			"noteId", "purposeType", "propertyType", "priceType", "isScraped"
-		).containsExactly(
-			expectedId,
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT,
-			false
+		assertAll(
+			() -> assertThat(note.purposeType()).isEqualTo(LimjangPurpose.RESIDENTIAL_PURPOSE),
+			() -> assertThat(note.propertyType()).isEqualTo(LimjangPropertyType.APARTMENT),
+			() -> assertThat(note.priceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+			() -> assertThat(note.price()).isEqualTo("50000"),
+			() -> assertThat(note.monthlyRent()).isEqualTo("4000")
 		);
-
-		assertThat(note.price()).isEqualTo("50000");
-		assertThat(note.monthlyRent()).isEqualTo("4000");
 	}
 
 	private void assertInitNoteEmptyFields(UserNotesGetResponse.UserNoteResponse note) {
-		assertThat(note)
-			.extracting("address", "shortAddress", "pyong", "floor", "rate")
-			.containsOnlyNulls();
-
-		assertThat(note.imageUrl()).isEmpty();
+		assertAll(
+			() -> assertThat(note.address()).isNull(),
+			() -> assertThat(note.shortAddress()).isNull(),
+			() -> assertThat(note.pyong()).isNull(),
+			() -> assertThat(note.floor()).isNull(),
+			() -> assertThat(note.rate()).isNull(),
+			() -> assertThat(note.imageUrl()).isEmpty()
+		);
 	}
 
 	private void flushAndTestUsers() {
@@ -191,5 +268,4 @@ public class InitNoteTest extends IntegrationTestSupport {
 
 		memberRepository.flush();
 	}
-
 }
--- src/test/java/umc/th/juinjang/api/limjang/service/command/NoteCreateTest.java
+++ src/test/java/umc/th/juinjang/api/limjang/service/command/NoteCreateTest.java
@@ -1,21 +1,32 @@
 package umc.th.juinjang.api.limjang.service.command;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import umc.th.juinjang.api.IntegrationTestSupport;
 import umc.th.juinjang.api.limjang.controller.parameter.LimjangSortOptions;
-import umc.th.juinjang.api.limjang.controller.request.NotePostRequest;
+import umc.th.juinjang.api.limjang.controller.request.LimjangsDeleteRequest;
+import umc.th.juinjang.api.limjang.controller.request.NoteInitRequest;
+import umc.th.juinjang.api.limjang.controller.request.NotePatchRequest;
+import umc.th.juinjang.api.limjang.service.LimjangCommandService;
 import umc.th.juinjang.api.limjang.service.NoteCommandServiceV2;
 import umc.th.juinjang.api.limjang.service.NoteQueryServiceV2;
 import umc.th.juinjang.api.limjang.service.response.NotePostResponse;
 import umc.th.juinjang.api.limjang.service.response.UserNoteGetResponse;
 import umc.th.juinjang.api.limjang.service.response.UserNotesGetResponse;
+import umc.th.juinjang.common.exception.handler.LimjangHandler;
+import umc.th.juinjang.domain.limjang.model.Limjang;
 import umc.th.juinjang.domain.limjang.model.LimjangPriceType;
 import umc.th.juinjang.domain.limjang.model.LimjangPropertyType;
 import umc.th.juinjang.domain.limjang.model.LimjangPurpose;
@@ -24,49 +35,207 @@ import umc.th.juinjang.domain.member.model.Member;
 import umc.th.juinjang.domain.member.repository.MemberRepository;
 
 @Slf4j
-class NoteCreateTest extends IntegrationTestSupport {
-
-	Member firstMember;
-	Member secondMember;
-	Member thirdMember;
-
-	NotePostResponse response;
+public class NoteCreateTest extends IntegrationTestSupport {
 
 	@Autowired
 	private MemberRepository memberRepository;
+	@Autowired
+	private NoteCommandServiceV2 noteCommandService;
+	@Autowired
+	private LimjangCommandService limjangCommandService;
 
 	@Autowired
-	private NoteCommandServiceV2 noteCommandServiceV2;
-
+	private NoteQueryServiceV2 noteQueryService;
 	@Autowired
 	private LimjangRepository limjangRepository;
-	@Autowired
-	private NoteQueryServiceV2 noteQueryServiceV2;
+
+	private Member firstMember;
+	private Member secondMember;
+	private Member thirdMember;
 
 	@BeforeEach
-	void setup() {
-		// given
+	void setUp() {
 		flushAndTestUsers();
+	}
 
-		NotePostRequest request = new NotePostRequest(
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT,
-			"50000",
-			"4000",
-			"서울특별시 강남구 테헤란로 123",      // roadAddress (필수)
-			"101호",                              // addressDetail
-			"1168010100",                         // bcode (필수)
-			"우리집 매물노트",                     // nickname (필수)
-			"5",                                  // floor (필수)
-			30,                                   // pyong
-			"서울특별시",                          // sido
-			"강남구",                             // sigungu
-			"역삼동",                             // bname1
-			""                                    // bname2
+	@DisplayName("새로운 노트를 생성할 때, ")
+	@Nested
+	class CreateNote {
+
+		private NotePostResponse noteInitResponse;
+
+		@BeforeEach
+		void setUpNote() {
+			// given
+			NoteInitRequest request = new NoteInitRequest(
+				LimjangPurpose.RESIDENTIAL_PURPOSE,
+				LimjangPropertyType.APARTMENT,
+				LimjangPriceType.MONTHLY_RENT,
+				"50000",
+				"4000"
+			);
+
+			// when
+			noteInitResponse = noteCommandService.initNote(request, firstMember);
+		}
+
+		@DisplayName("올바른 요청이 주어지면, 정상적으로 생성된다")
+		@Test
+		void createsNote_whenValidRequestIsProvided() {
+			// given
+			Long createNoteId = noteInitResponse.noteId();
+
+			// when
+			Limjang note = limjangRepository.findById(createNoteId).orElseThrow();
+
+			// then
+			assertAll(
+				() -> assertThat(note.getMemberId().getMemberId()).isEqualTo(firstMember.getMemberId()),
+				() -> assertThat(note.getPurpose()).isEqualTo(LimjangPurpose.RESIDENTIAL_PURPOSE),
+				() -> assertThat(note.getPropertyType()).isEqualTo(LimjangPropertyType.APARTMENT),
+				() -> assertThat(note.getPriceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+				() -> assertThat(note.getRecordCount()).isEqualTo(0),
+				() -> assertThat(note.getLimjangPrice()).isNotNull(),
+				() -> assertThat(note.getNickname()).endsWith(" 매물노트")
+			);
+		}
+
+		@DisplayName("생성된 노트가 리스트에 정상적으로 노출된다")
+		@Test
+		void createdNoteIsVisible_inList() {
+			// given
+			Long createdNoteId = noteInitResponse.noteId();
+			log.info("생성된 노트 ID: {}", createdNoteId);
+
+			// when
+			log.info("=== 노트 조회 시작 ===");
+			UserNotesGetResponse response = noteQueryService.findUsersNotes(
+				firstMember, LimjangSortOptions.CREATED, ""
+			);
+			log.info("조회된 노트 개수: {}", response.notes().size());
+
+			// then
+			assertThat(response.notes()).hasSize(1);
+
+			UserNotesGetResponse.UserNoteResponse note = response.notes().get(0);
+			log.info("=== 노트 정보 ===");
+			log.info("### notes : {} ", note);
+			log.info("노트 ID: {}", note.noteId());
+			log.info("노트 이름: {}", note.name());
+			log.info("가격: {}", note.price());
+			log.info("월세: {}", note.monthlyRent());
+
+			assertAll(
+				() -> assertInitNoteBasicFields(note, createdNoteId),
+				() -> assertInitNoteEmptyFields(note)
+			);
+		}
+
+		@DisplayName("생성된 노트의 상세가 정상적으로 노출된다")
+		@Test
+		void createdNoteDetailIsVisible() {
+			// given
+			Long createdNoteId = noteInitResponse.noteId();
+
+			// when
+			UserNoteGetResponse response = noteQueryService.findNote(createdNoteId);
+			log.info("### response : {} ", response);
+
+			// then
+			assertInitNoteBasicFields(response, createdNoteId);
+		}
+
+		@DisplayName("생성된 노트가 정상적으로 수정된다")
+		@Test
+		@Transactional
+		void updateNoteSuccess_whenCreateNote() {
+			// given
+			Long createNoteId = noteInitResponse.noteId();
+
+			NotePatchRequest updateRequest = new NotePatchRequest(
+				LimjangPriceType.MONTHLY_RENT,
+				"60000",  // 보증금 변경
+				"5000",   // 월세 변경
+				"서울특별시 강남구 테헤란로 123",  // 도로명 주소
+				"101동 101호",  // 상세 주소
+				"1168010100",   // 법정동코드
+				"강남 아파트 매물노트",  // 닉네임 변경
+				"5",      // 층수
+				32,       // 평수
+				"서울특별시",   // 시도
+				"강남구",       // 시군구
+				"역삼동",       // 법정동명1
+				""              // 법정동명2
+			);
+
+			// when
+			noteCommandService.updateNoteV2(createNoteId, updateRequest);
+
+			// then
+			Limjang updatedNote = limjangRepository.findById(createNoteId).orElseThrow();
+
+			assertAll(
+				() -> assertThat(updatedNote.getNickname()).isEqualTo("강남 아파트 매물노트"),
+				() -> assertThat(updatedNote.getPriceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+				() -> assertThat(updatedNote.getFloor()).isEqualTo("5"),
+				() -> assertThat(updatedNote.getPyong()).isEqualTo(32),
+				() -> assertThat(updatedNote.getLimjangPrice()).isNotNull(),
+				() -> assertThat(updatedNote.getLimjangPrice().getMonthlyRent()).isEqualTo("5000"),
+				() -> assertThat(updatedNote.getAddressEntity()).isNotNull(),
+				() -> assertThat(updatedNote.getAddressEntity().getRoadAddress()).isEqualTo("서울특별시 강남구 테헤란로 123"),
+				() -> assertThat(updatedNote.getAddressEntity().getAddressDetail()).isEqualTo("101동 101호")
+			);
+		}
+
+		@DisplayName("생성된 노트가 정상적으로 삭제된다")
+		@Test
+		void deleteNoteSuccessCreateByNote() {
+			// given
+			long createNoteId = noteInitResponse.noteId();
+			List<Long> noteIds = new ArrayList<>();
+			noteIds.add(createNoteId);
+
+			// when
+			limjangCommandService.deleteLimjangs(LimjangsDeleteRequest.of(noteIds), firstMember);
+
+			// then
+			assertThrows(LimjangHandler.class, () -> noteQueryService.findNote(createNoteId));
+		}
+	}
+
+	// Helper methods
+	private void assertInitNoteBasicFields(UserNotesGetResponse.UserNoteResponse note, Long expectedId) {
+		assertAll(
+			() -> assertThat(note.noteId()).isEqualTo(expectedId),
+			() -> assertThat(note.purposeType()).isEqualTo(LimjangPurpose.RESIDENTIAL_PURPOSE),
+			() -> assertThat(note.propertyType()).isEqualTo(LimjangPropertyType.APARTMENT),
+			() -> assertThat(note.priceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+			() -> assertThat(note.isScraped()).isFalse(),
+			() -> assertThat(note.name()).endsWith(" 매물노트"),
+			() -> assertThat(note.price()).isEqualTo("50000"),
+			() -> assertThat(note.monthlyRent()).isEqualTo("4000")
 		);
+	}
 
-		response = noteCommandServiceV2.createNote(request, firstMember);
+	private void assertInitNoteBasicFields(UserNoteGetResponse note, Long expectedId) {
+		assertAll(
+			() -> assertThat(note.purposeType()).isEqualTo(LimjangPurpose.RESIDENTIAL_PURPOSE),
+			() -> assertThat(note.propertyType()).isEqualTo(LimjangPropertyType.APARTMENT),
+			() -> assertThat(note.priceType()).isEqualTo(LimjangPriceType.MONTHLY_RENT),
+			() -> assertThat(note.price()).isEqualTo("50000"),
+			() -> assertThat(note.monthlyRent()).isEqualTo("4000")
+		);
+	}
+
+	private void assertInitNoteEmptyFields(UserNotesGetResponse.UserNoteResponse note) {
+		assertAll(
+			() -> assertThat(note.address()).isNull(),
+			() -> assertThat(note.shortAddress()).isNull(),
+			() -> assertThat(note.pyong()).isNull(),
+			() -> assertThat(note.floor()).isNull(),
+			() -> assertThat(note.rate()).isNull(),
+			() -> assertThat(note.imageUrl()).isEmpty()
+		);
 	}
 
 	private void flushAndTestUsers() {
@@ -100,45 +269,5 @@ class NoteCreateTest extends IntegrationTestSupport {
 		memberRepository.save(thirdMember);
 
 		memberRepository.flush();
-	}
-
-	@Test
-	@DisplayName("createNote 를 통해, note를 만들었을 경우에, 상세가 정상적으로 노출되는 가?")
-	void isDetailShowByCreateNote() {
-		Long createNoteId = response.noteId();
-
-		// given
-		UserNoteGetResponse note = noteQueryServiceV2.findNote(createNoteId);
-		assertThat(note).extracting(
-			"purposeType",
-			"propertyType",
-			"priceType"
-		).containsExactly(
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT
-		);
-
-	}
-
-	@Test
-	@DisplayName("createNote 를 통해, 노트를 만들었을 경우에 리스트가 정상적으로 노출되는 가 ?")
-	void createNoteByList() {
-		// given
-		UserNotesGetResponse response = noteQueryServiceV2.findUsersNotes(firstMember, LimjangSortOptions.CREATED, "");
-		log.info("#### NOTES  : {} ", response.notes());
-		// then
-		assertThat(response.notes()).hasSize(1);
-		assertThat(response.notes().get(0)).extracting(
-			"purposeType",
-			"propertyType",
-			"priceType",
-			"name"
-		).containsExactly(
-			LimjangPurpose.RESIDENTIAL_PURPOSE,
-			LimjangPropertyType.APARTMENT,
-			LimjangPriceType.MONTHLY_RENT,
-			"우리집 매물노트"
-		);
 	}
 }
