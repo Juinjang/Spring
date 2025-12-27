4b1824a2238ff76e773f35329a50b5464a5b1c68
essaysir <sonjs7554@naver.com>
2025-12-27 14:18:49 +0900

    feat : 애플리케이션 타임존 수정

--- src/main/java/umc/th/juinjang/JuinjangApplication.java
+++ src/main/java/umc/th/juinjang/JuinjangApplication.java
@@ -1,12 +1,15 @@
 package umc.th.juinjang;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 // @EnableAsync
@@ -14,6 +17,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableRetry
 public class JuinjangApplication {
+
+	@PostConstruct
+	public void init() {
+		// set timezone
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JuinjangApplication.class, args);
